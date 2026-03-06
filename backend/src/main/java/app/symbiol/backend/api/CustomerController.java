303912405345c39ddad2334752c6117208130494
David <74102444+daverlon@users.noreply.github.com>
2026-03-06 14:08:27 +0800

    connect frontend to backend

--- backend/src/main/java/app/symbiol/backend/api/CustomerController.java
+++ backend/src/main/java/app/symbiol/backend/api/CustomerController.java
@@ -1,5 +1,6 @@
 package app.symbiol.backend.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import app.symbiol.backend.service.CustomerService;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api")
 public class CustomerController {
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react"
+import { createCustomer } from "../api/customerApi"
+
+export default function Login() {
+
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  async function handleLogin(e: React.SubmitEvent) {
+    e.preventDefault()
+    try {
+        const result = await createCustomer(username, password)
+        console.log("login result:", result)
+    } catch (err) {
+        console.error ("login failed", err)
+    }
+
+
+    // try {
+    //   const result = await login(username, password)
+    //   console.log("login result:", result)
+
+    //   // later you might store JWT
+    //   // localStorage.setItem("token", result.token)
+
+    // } catch (err) {
+    //   console.error("login failed", err)
+    // }
+  }
+
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-gray-200">
+
+      <form
+        onSubmit={handleLogin}
+        className="bg-white p-8 rounded-xl shadow-md w-80"
+      >
+
+        <h1 className="text-xl mb-6 font-semibold">Login</h1>
+
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="border w-full p-2 mb-4 rounded"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="border w-full p-2 mb-4 rounded"
+        />
+
+        <button
+          type="submit"
+          className="bg-blue-500 text-white w-full p-2 rounded"
+        >
+          Login
+        </button>
+
+      </form>
+    </div>
+  )
+}
