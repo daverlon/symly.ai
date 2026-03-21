5779f52ed246e2a4ac34ff69869dcc33e1d85155
David <74102444+daverlon@users.noreply.github.com>
2026-03-21 22:31:09 +0800

    start of upload session token to upload session jwt exchange

---
+++ backend/src/main/java/app/symbiol/backend/repository/UploadKeyRepository.java
@@ -0,0 +1,14 @@
+package app.symbiol.backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import app.symbiol.backend.model.UploadSessionKey;
+
+@Repository
+public interface UploadKeyRepository extends JpaRepository<UploadSessionKey, Long> {
+
+    public Optional<UploadSessionKey> findByKey(String key);
+}
