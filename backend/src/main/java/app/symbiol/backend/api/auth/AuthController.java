ce553cf667312ae8aad93b53088b4c0339cdddc6
David <74102444+daverlon@users.noreply.github.com>
2026-03-29 22:46:23 +0800

    restore deleted controller

---
+++ backend/src/main/java/app/symbiol/backend/api/auth/AuthController.java
@@ -0,0 +1,39 @@
+package app.symbiol.backend.api.auth;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.symbiol.backend.dto.UserDto;
+import app.symbiol.backend.security.JwtService;
+import io.jsonwebtoken.JwtException;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final JwtService jwtService;
+
+    public AuthController(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @GetMapping
+    @RequestMapping("/verify")
+    public ResponseEntity<UserDto> verifyToken(@RequestHeader("Authorization") String authHeader) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        String token = authHeader.substring(7);
+        try {
+            String username = jwtService.validateTokenAndGetUsername(token);
+            return ResponseEntity.ok().body(new UserDto(username));
+        } catch (JwtException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+}
