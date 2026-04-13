7450214023dd222e156d7dc5862ccf0e65f721ae
daverlon <david.wip.long@gmail.com>
2026-04-13 23:33:22 +0800

    clean imports

--- backend/src/main/java/app/symbiol/backend/api/DeskController.java
+++ backend/src/main/java/app/symbiol/backend/api/DeskController.java
@@ -3,7 +3,6 @@ package app.symbiol.backend.api;
 import java.util.List;
 import java.util.stream.IntStream;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.symbiol.backend.dto.DeskImageDto;
